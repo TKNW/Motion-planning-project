4a8862afd69a68cddaf1c37c1638cc5e314e2b51
TKNW <kyon1.048596@gmail.com>
2023-11-17 19:39:55 +0800

    Add scripts

---
+++ Motion planning project/Assets/Scripts/RobotAgent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class RobotAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void OnEpisodeBegin()
+    {
+
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
+    }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
